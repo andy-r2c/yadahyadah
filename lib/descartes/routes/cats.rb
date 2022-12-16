c3e18d00a4d2664c18460515279e1929eda60ed6
andy-r2c <100232702+andy-r2c@users.noreply.github.com>
2022-12-16 13:12:08 -0500

    Update cats.rb

--- lib/descartes/routes/cats.rb
+++ lib/descartes/routes/cats.rb
@@ -6,8 +6,8 @@ module Descartes
         content_type 'application/json'
         @cats = []
         begin
-          doc = Nokogiri::HTML(open("http://thecatapi.com/api/images/get?format=xml&size=small&results_per_page=#{params[:count]}"))
-          doc.css('url').each do |kitty|
+          docs = Nokogiri::HTML(open("http://thecatapi.com/api/images/get?format=xml&size=small&results_per_page=#{params[:count]}"))
+          docs.css('url').each do |kitty|
             @cats << kitty.content
           end
         rescue => e
